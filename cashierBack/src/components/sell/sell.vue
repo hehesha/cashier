87ad27b5c24fcbfcc6a1bf00dfde1813b830d986
unknown <chenleshi@mytophome.com>
2018-05-14 18:50:57 +0800

    收银页面

--- cashierBack/src/components/sell/sell.vue
+++ cashierBack/src/components/sell/sell.vue
@@ -18,17 +18,30 @@
                     <th>金额</th>
                 </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+                <tr v-for="(item,index) in dataset">
+                    <td>{{index+1}}</td>
+                    <td v-for="(value,key) in item">{{value}}</td>
+                    <td>{{item.price*item.num}}</td>
+                </tr>
+            </tbody>
           </table>
           </div>
       </div>
       <div class="s-footer">
         <div class="f-left fl">
-        <span>商品名</span>
-        <span>价格</span>
-        <span>数量</span><el-input-number v-model="num1" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
+        <label>商品条码
+          <input type="text" @keyup.enter="getDetails($event)" />
+        </label></br>
+        商品名：<span v-if="dataset.length>0">{{dataset[currentL].name}}</span></br>
+        价格：<span v-if="dataset.length>0">{{dataset[currentL].price}}</span></br>
+        <span>数量</span><el-input-number v-model="num1" :step="1" @change="changeNumber"></el-input-number>
         </div>
-        <div class="f-right fr"></div>
+        <div class="f-right fr">
+          <div>商品总数：<span>{{dataset.length}}</span></div>
+          <div>总金额：<span>{{total}}</span></div> 
+            
+        </div>
       </div>
     </div>
 </template>
@@ -38,8 +51,32 @@ import './sell.scss'
 export default {
   data () {
     return {
-        num1: 1
+        
+        dataset:[],
+        currentL:0,
+        num1: 1,
+        total:0
     }
+  },
+  methods:{
+      getDetails(e){
+          console.log(e.target.value);
+          // 发起请求
+         this.num1=1;
+         var res={"name":"写字笔","price":"59","num":"1","unit":"支","descount":"1"};
+         this.dataset.push(res);
+         this.currentL=this.dataset.length-1;
+         e.target.value="";
+
+      },
+      changeNumber(){
+        console.log(this.num1);
+        this.dataset[this.currentL].num=this.num1;
+      },
+      getTotals(arr){
+        
+      }
+     
   }
 
 }
